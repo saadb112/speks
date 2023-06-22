acbd24b2a159c479af602e151b6ce05b1762a5c1
Saad Baig <saadbaig789@gmail.com>
2023-06-22 19:06:41 +0500

    updated

--- api/index.js
+++ api/index.js
@@ -6,6 +6,17 @@ const cors = require("cors")
 const path = require("path")
 const Dir = path.join(__dirname, "../client")
 const port = process.env.PORT || 8000
+const multer = require("multer")
+const storage = multer.diskStorage({
+    destination : (req,file,cb)=>{
+        cb(null, "./Images")
+    },
+    filename : (req,file,cb)=>{
+        console.log(file.originalname)
+        cb(null, Date.now + path.extname(file.originalname))
+    }
+})
+const upload = multer({storage})
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 // parse application/json
@@ -14,6 +25,12 @@ app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.static(Dir))
 app.get("/", ()=>{
+    res.send("hello")
+  })
+app.get("/upload", ()=>{
+    res.send("hello")
+  })
+app.post("/upload", upload.single("image"),(req,res)=>{
     res.send("hello")
   })
   app.listen(port, ()=>{
